2e362a8035195950c66dac553d7dc54ac8017ec3
Jakub Oczkowski <jakuboczkowski2@gmail.com>
2019-04-16 01:22:52 +0200

    ordering added

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,6 +2,7 @@ class MoviesController < ApplicationController
   before_action :authenticate_user!, only: [:send_info]
 
   def index
+    return @movies = resolve_case(params[:case]) if params.has_key?(:case)
     @movies = Movie.all.decorate
   end
 
@@ -20,4 +21,22 @@ class MoviesController < ApplicationController
     MovieExporter.new.call(current_user, file_path)
     redirect_to root_path, notice: "Movies exported"
   end
+
+  private
+
+  # rubocop:disable Metrics/MethodLength
+  def resolve_case(sorting_type)
+    case sorting_type
+    when "desc"
+      soretd_movies = Movie.all.order(title: :desc).decorate
+    when "asc"
+      soretd_movies = Movie.all.order(:title).decorate
+    when "newest"
+      soretd_movies = Movie.all.order(released_at: :desc).decorate
+    when "oldest"
+      soretd_movies = Movie.all.order(:released_at).decorate
+    end
+    soretd_movies
+  end
+  # rubocop:enable Metrics/MethodLength
 end
