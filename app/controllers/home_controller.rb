504098f2f227fb352b03030a416dc7228ecc0ca2
Jakub Oczkowski <jakuboczkowski2@gmail.com>
2019-04-15 23:54:10 +0200

    views, action up

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,11 @@
 class HomeController < ApplicationController
   def welcome; end
+
+  def top_ten
+    @top_ten = User.left_joins(:comments)
+      .group(:id, :create_at)
+      .order("COUNT(comments.id) DESC")
+      .where("comments.created_at > ?", 7.days.ago)
+      .limit(10)
+  end
 end
